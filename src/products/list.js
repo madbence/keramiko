dbc20769e8b729e6c4be6f64f0ce48a00b5937ab
Bence Dányi <bence@danyi.me>
2017-07-16 00:22:32 +0200

    feat: make ProductList stateful

--- src/products/list.js
+++ src/products/list.js
@@ -1,4 +1,6 @@
 import Inferno from 'inferno';
+import Component from 'inferno-component';
+import {fetch} from './';
 
 const id = x => x;
 const f = (name, label, display = id) => ({name, label, display});
@@ -11,33 +13,63 @@ const productFields = [
   f('tags', 'Címkék', tags => tags.join(', ')),
 ];
 
-export default ({items, loading}) => (
-  <div className='card'>
-    <header>
-      <h2>Termékek</h2>
-      <button>Új termék</button>
-    </header>
-    <ul className='products'>
-      <li className='header product'>
-      {
-        productFields.map(({name, label}) => (
-          <div className={'field ' + name}>{label}</div>
-        ))
-      }
-      </li>
-      {
-        loading
-          ? <li className='product loader'>Betöltés...</li>
-          : items.map(item => (
-            <li className='product'>
-            {
-              productFields.map(({name, display}) => (
-                <div className={'field ' + name}>{display(item[name])}</div>
+export default class ProductList extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      loading: false,
+      items: [],
+    };
+    this.router = context.router;
+  }
+
+  componentDidMount() {
+    this.loadProducts();
+  }
+
+  async loadProducts() {
+    this.setState({
+      loading: true,
+      items: [],
+    });
+    const items = await fetch();
+    this.setState({
+      loading: false,
+      items,
+    });
+  }
+
+  render() {
+    const {loading, items} = this.state;
+    return (
+      <div className='card'>
+        <header>
+          <h2>Termékek</h2>
+          <button onClick={() => this.router.push('/products/1')}>Új termék</button>
+        </header>
+        <ul className='products'>
+          <li className='header product'>
+          {
+            productFields.map(({name, label}) => (
+              <div className={'field ' + name}>{label}</div>
+            ))
+          }
+          </li>
+          {
+            loading
+              ? <li className='product loader'>Betöltés...</li>
+              : items.map(item => (
+                <li className='product'>
+                {
+                  productFields.map(({name, display}) => (
+                    <div className={'field ' + name}>{display(item[name])}</div>
+                  ))
+                }
+                </li>
               ))
-            }
-            </li>
-          ))
-      }
-    </ul>
-  </div>
-);
+          }
+        </ul>
+      </div>
+    );
+  }
+}
