6f0fd7bcd14a5de09c9b2bbbbfd4c77b0a23de58
Bence Dányi <bence@danyi.me>
2017-07-16 00:23:49 +0200

    feat: add simple routing

--- src/app.js
+++ src/app.js
@@ -1,57 +1,32 @@
 import Inferno from 'inferno';
-import Component from 'inferno-component';
-import {List as ProductList, fetch as fetchProducts} from './products';
+import {Router, Route} from 'inferno-router';
+import createBrowserHistory from 'history/createBrowserHistory';
+import {List as ProductList, Details as ProductDetails} from './products';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      products: {
-        fetching: false,
-        items: [],
-      },
-    };
-  }
-
-  componentDidMount() {
-    this.fetchProducts();
-  }
-
-  async fetchProducts() {
-    this.setState({
-      products: {
-        fetching: true,
-        items: [],
-      },
-    });
-    const items = await fetchProducts();
-    this.setState({
-      products: {
-        fetching: false,
-        items,
-      },
-    });
-  }
-
-  render() {
-    return (
-      <div id='wrapper'>
-        <div id='nav'>
-          <ul>
-            <li>Terméklista</li>
-          </ul>
-        </div>
-        <div id='main-wrapper'>
-          <header>
-            <h2>Szia!</h2>
-          </header>
-          <div id='main'>
-            <ProductList loading={this.state.products.fetching} items={this.state.products.items} />
-          </div>
-        </div>
+const App = props => (
+  <div id='wrapper'>
+    <div id='nav'>
+      <ul>
+        <li>Terméklista</li>
+      </ul>
+    </div>
+    <div id='main-wrapper'>
+      <header>
+        <h2>Szia!</h2>
+      </header>
+      <div id='main'>
+        {props.children}
       </div>
-    );
-  }
-}
+    </div>
+  </div>
+);
 
-Inferno.render(<App />, document.querySelector('#app'));
+Inferno.render(
+  <Router history={createBrowserHistory()}>
+    <Route component={App}>
+      <Route path='/' component={ProductList} />
+      <Route path='/products/:id' component={({params}) => <ProductDetails id={params.id} />} />
+    </Route>
+  </Router>,
+  document.querySelector('#app')
+);
