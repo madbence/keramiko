289a14fe2c75ec527160273674ddb0b9b552da70
Bence Dányi <bence@danyi.me>
2017-07-27 20:54:29 +0200

    chore: add initial sql seed

---
+++ src/seed.sql
@@ -0,0 +1,10 @@
+drop table if exists products;
+
+create table products (
+  id serial primary key,
+  name varchar(32) not null,
+  price integer not null,
+  description text not null,
+  createdAt timestamp not null default current_timestamp,
+  updatedAt timestamp not null default current_timestamp
+);
