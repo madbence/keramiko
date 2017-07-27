b237fdfb3401a1e7749dd17761a7b4bec0a0b675
Bence Dányi <bence@danyi.me>
2017-07-27 20:59:29 +0200

    refactor: remove extra braces

--- src/server/middlewares/file.js
+++ src/server/middlewares/file.js
@@ -4,9 +4,7 @@ import {get} from 'koa-route';
 export default (url, path, type) => get(url, async ctx => {
   const buffer = await new Promise((resolve, reject) => {
     fs.readFile(path, (err, buffer) => {
-      if (err) {
-        return reject(err);
-      }
+      if (err) return reject(err);
       resolve(buffer);
     });
   });
