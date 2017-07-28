336a53066eabae0de8394fb96242b05ffe98e5bd
Bence Dányi <bence@danyi.me>
2017-07-28 21:34:01 +0200

    refactor: move api routes to separate module

--- src/server/app.js
+++ src/server/app.js
@@ -1,37 +1,7 @@
 import Koa from 'koa';
-import {post, put, get} from 'koa-route';
 
-import json from './utils/json';
 import serve from './middlewares/file';
-
-import {createProduct, updateProduct, getProducts, getProductById} from './products';
-
-const create = async ctx => {
-  const body = await json(ctx.req);
-  ctx.body = await createProduct({
-    name: body.name,
-    price: body.price,
-    description: body.description,
-  });
-}
-
-const update = async (ctx, id) => {
-  const body = await json(ctx.req);
-  ctx.body = await updateProduct({
-    id,
-    name: body.name,
-    price: body.price,
-    description: body.description,
-  });
-}
-
-const list = async ctx => {
-  ctx.body = await getProducts();
-};
-
-const fetch = async (ctx, id) => {
-  ctx.body = await getProductById(id);
-};
+import api from './routes';
 
 const app = new Koa();
 
@@ -39,12 +9,7 @@ app
 
   .use(serve('/bundle.js', './public/bundle.js', 'application/javascript'))
   .use(serve('/bundle.css', './public/bundle.css', 'text/css'))
-
-  .use(get('/api/v1/products', list))
-  .use(get('/api/v1/products/:id', fetch))
-  .use(put('/api/v1/products/:id', update))
-  .use(post('/api/v1/products', create))
-
+  .use(api)
   .use(serve('/*', './public/index.html', 'text/html'));
 
 app.listen(3000);
---
+++ src/server/routes/index.js
@@ -0,0 +1,40 @@
+import compose from 'koa-compose';
+import {post, put, get} from 'koa-route';
+
+import json from '../utils/json';
+
+import {createProduct, updateProduct, getProducts, getProductById} from '../products';
+
+const create = async ctx => {
+  const body = await json(ctx.req);
+  ctx.body = await createProduct({
+    name: body.name,
+    price: body.price,
+    description: body.description,
+  });
+}
+
+const update = async (ctx, id) => {
+  const body = await json(ctx.req);
+  ctx.body = await updateProduct({
+    id,
+    name: body.name,
+    price: body.price,
+    description: body.description,
+  });
+}
+
+const list = async ctx => {
+  ctx.body = await getProducts();
+};
+
+const fetch = async (ctx, id) => {
+  ctx.body = await getProductById(id);
+};
+
+export default compose([
+  get('/api/v1/products', list),
+  get('/api/v1/products/:id', fetch),
+  put('/api/v1/products/:id', update),
+  post('/api/v1/products', create),
+]);
