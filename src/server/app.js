2379f1676ac8848473e0f1bf520c5ffd8428b98c
Bence Dányi <bence@danyi.me>
2017-08-03 22:41:24 +0200

    feat: serve admin portal on different subdomain

--- src/server/app.js
+++ src/server/app.js
@@ -2,14 +2,34 @@ import Koa from 'koa';
 
 import serve from './middlewares/file';
 import api from './routes';
+import compose from 'koa-compose';
+import {get} from 'koa-route';
+
+const subdomain = (domain, middlewares) => async (ctx, next) => {
+  if (ctx.subdomains[0] === domain) {
+    return middlewares(ctx, next);
+  }
+  return next();
+};
 
 const app = new Koa();
 
-app
+const admin = compose([
+  serve('/bundle.js', './public/bundle.js', 'application/javascript'),
+  serve('/bundle.css', './public/bundle.css', 'text/css'),
+  api,
+  serve('/*', './public/index.html', 'text/html'),
+]);
 
-  .use(serve('/bundle.js', './public/bundle.js', 'application/javascript'))
-  .use(serve('/bundle.css', './public/bundle.css', 'text/css'))
-  .use(api)
-  .use(serve('/*', './public/index.html', 'text/html'));
+const store = compose([
+  get('/*', ctx => {
+    ctx.body = 'Hello world!';
+  }),
+]);
+
+app
+  .use(subdomain('admin', admin))
+  .use(store);
+
 
 app.listen(3000);
