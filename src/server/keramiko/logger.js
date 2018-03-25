605fe7add0577de71cb94d91870745fe5c202b1c
Bence Dányi <bence@danyi.me>
2018-03-25 23:54:54 +0200

    feat: serialize errors in the logger

--- src/server/keramiko/logger.js
+++ src/server/keramiko/logger.js
@@ -4,6 +4,16 @@ configure({
   handler: record => {
     record.date = new Date();
 
+    if (record.error) {
+      const error = record.error;
+      record.error = {
+        message: error.message,
+        name: error.name,
+        stack: error.stack,
+      };
+      if (!record.message) record.message = error.message;
+    }
+
     // eslint-disable-next-line no-console
     console.log(`[${record.date.toISOString()}][${record.name}][${record.level}] ${record.message}`);
   },
