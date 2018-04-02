22e06e7f40440bb0f776adb8c33564018c6a21b3
Bence Dányi <bence@danyi.me>
2018-04-02 02:12:07 +0200

    feat: add create method for the product service

--- src/server/keramiko/config.js
+++ src/server/keramiko/config.js
@@ -1,5 +1,7 @@
 import config from 'common/config';
 import DatabaseService from 'common/db';
+import {EventRepository} from 'common/event';
+
 import createLogger from 'keramiko/logger';
 import ProductRepository from 'keramiko/models/product';
 
@@ -8,7 +10,8 @@ const db = new DatabaseService({
   connectionString: `postgres://${config.db.user}@${config.db.host}`,
   logger: logger.child('db'),
 });
+const store = new EventRepository({db});
 
-const products = new ProductRepository({db});
+const products = new ProductRepository({db, store});
 
 export {products, logger, db};
--- src/server/keramiko/models/product.js
+++ src/server/keramiko/models/product.js
@@ -8,8 +8,45 @@ const parse = row => ({
 });
 
 export default class ProductRepository {
-  constructor({db}) {
+  constructor({db, store}) {
     this.db = db;
+    this.store = store;
+  }
+
+  async generateId() {
+    const result = await this.db.query('select nextval($1) as id', ['products_id_seq']);
+    return result.rows[0].id;
+  }
+
+  async create(props) {
+    const id = await this.generateId();
+    const now = new Date();
+
+    await this.store.append({
+      type: 'product',
+      id,
+      version: 0,
+      createdAt: now,
+      details: {
+        id,
+        name: props.name,
+        price: props.price,
+        description: props.description,
+      },
+      meta: {
+        event: 'created',
+      },
+    });
+
+    await this.db.query('insert into products (id, name, price, description, "createdAt", "updatedAt", "deletedAt") values ($1, $2, $3, $4, $5, $6, $7)', [
+      id,
+      props.name,
+      props.price,
+      props.description,
+      now,
+      now,
+      null,
+    ]);
   }
 
   async getById(id) {
