3e07e760f80de9306c8771c3ade3b3bbab779f71
Bence Dányi <bence@danyi.me>
2018-04-02 02:21:50 +0200

    feat: use transactions to ensure valid state
    if an insertion fails, the whole transaction is rollbacked

--- src/server/keramiko/models/product.js
+++ src/server/keramiko/models/product.js
@@ -22,31 +22,33 @@ export default class ProductRepository {
     const id = await this.generateId();
     const now = new Date();
 
-    await this.store.append({
-      type: 'product',
-      id,
-      version: 0,
-      createdAt: now,
-      details: {
+    await this.db.transaction(async db => {
+      await db.query('insert into products (id, name, price, description, "createdAt", "updatedAt", "deletedAt") values ($1, $2, $3, $4, $5, $6, $7)', [
         id,
-        name: props.name,
-        price: props.price,
-        description: props.description,
-      },
-      meta: {
-        event: 'created',
-      },
-    });
+        props.name,
+        props.price,
+        props.description,
+        now,
+        now,
+        null,
+      ]);
 
-    await this.db.query('insert into products (id, name, price, description, "createdAt", "updatedAt", "deletedAt") values ($1, $2, $3, $4, $5, $6, $7)', [
-      id,
-      props.name,
-      props.price,
-      props.description,
-      now,
-      now,
-      null,
-    ]);
+      await this.store.append({
+        type: 'product',
+        id,
+        version: 0,
+        createdAt: now,
+        details: {
+          id,
+          name: props.name,
+          price: props.price,
+          description: props.description,
+        },
+        meta: {
+          event: 'created',
+        },
+      });
+    });
   }
 
   async getById(id) {
