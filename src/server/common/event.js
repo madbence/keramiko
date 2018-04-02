7ad7b9165e358e52732bdb67b5190107eb3ee52e
Bence Dányi <bence@danyi.me>
2018-04-02 02:08:50 +0200

    feat: add event store
    The event store is an append-only log for domain events.
    Every event belongs to a domain aggregate, which is identified by its
    type and id (`aggregateType`, `aggregateId`).
    The version field can be used to migitate concurrency issues.

---
+++ src/server/common/event.js
@@ -0,0 +1,18 @@
+class EventRepository {
+  constructor({db}) {
+    this.db = db;
+  }
+
+  append({type, id, version, createdAt, details, meta}) {
+    return this.db.query('insert into events ("aggregateType", "aggregateId", "details", "meta", "createdAt", "version") values ($1, $2, $3, $4, $5, $6) returning id, "createdAt"', [
+      type,
+      id,
+      details,
+      meta,
+      createdAt,
+      version,
+    ]);
+  }
+}
+
+export {EventRepository};
